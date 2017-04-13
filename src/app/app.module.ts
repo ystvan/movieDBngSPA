7f9b38fbe9282adeeb6fde60e4eaef6dcee87936
Istvan Marki <istv0050@edu.easj.dk>
2017-04-13 19:06:56 +0200

    let some space for navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,19 +2,35 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { RouterModule } from '@angular/router';
+
 
 import { AppComponent } from './app.component';
 import { SimplehttpComponent } from './simplehttp/simplehttp.component';
 
+import { HeaderComponent } from './header/header.component';
+
+
 @NgModule({
   declarations: [
     AppComponent,
-    SimplehttpComponent
+    SimplehttpComponent,
+    
+    HeaderComponent,
+    
+    
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    RouterModule.forRoot([
+            { path: '', redirectTo: 'home', pathMatch: 'full' },
+            { path: 'top-rated', component: SimplehttpComponent },
+            { path: 'search', component: SimplehttpComponent },
+            { path: 'fun', component: SimplehttpComponent },
+            { path: '**', redirectTo: 'hello' }
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
